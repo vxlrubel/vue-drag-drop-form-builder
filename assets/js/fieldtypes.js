d7af3ceb9a0fc92c9a36a8d2982a955047b726d2
Rubel Mahmud <vxlrubel@gmail.com>
2026-01-09 23:05:40 +0600

    import types from another file.

---
+++ assets/js/fieldtypes.js
@@ -0,0 +1,57 @@
+export const fieldTypes = [
+   {
+      type: "text",
+      label: "Text Input",
+      icon: "fas fa-font",
+   },
+   {
+      type: "email",
+      label: "Email Input",
+      icon: "fas fa-envelope",
+   },
+   {
+      type: "photo",
+      label: "Photo",
+      icon: "fas fa-camera",
+   },
+   {
+      type: "number",
+      label: "Number Input",
+      icon: "fas fa-hashtag",
+   },
+   {
+      type: "textarea",
+      label: "Text Area",
+      icon: "fas fa-align-left",
+   },
+   {
+      type: "select",
+      label: "Select Dropdown",
+      icon: "fas fa-caret-square-down",
+   },
+   {
+      type: "radio",
+      label: "Radio Button",
+      icon: "fas fa-dot-circle",
+   },
+   {
+      type: "checkbox",
+      label: "Checkbox",
+      icon: "fas fa-check-square",
+   },
+   {
+      type: "heading",
+      label: "Heading",
+      icon: "fas fa-heading",
+   },
+   {
+      type: "paragraph",
+      label: "Paragraph",
+      icon: "fas fa-paragraph",
+   },
+   {
+      type: "container",
+      label: "Container",
+      icon: "fas fa-layer-group",
+   },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+import { fieldTypes } from "./fieldtypes.js";
 const { createApp } = Vue;
 const draggable = window.vuedraggable;
 const EditForm = createApp({
@@ -6,63 +7,7 @@ const EditForm = createApp({
    },
    data() {
       return {
-         fieldTypes: [
-            {
-               type: "text",
-               label: "Text Input",
-               icon: "fas fa-font",
-            },
-            {
-               type: "email",
-               label: "Email Input",
-               icon: "fas fa-envelope",
-            },
-            {
-               type: "photo",
-               label: "Photo",
-               icon: "fas fa-camera",
-            },
-            {
-               type: "number",
-               label: "Number Input",
-               icon: "fas fa-hashtag",
-            },
-            {
-               type: "textarea",
-               label: "Text Area",
-               icon: "fas fa-align-left",
-            },
-            {
-               type: "select",
-               label: "Select Dropdown",
-               icon: "fas fa-caret-square-down",
-            },
-            {
-               type: "radio",
-               label: "Radio Button",
-               icon: "fas fa-dot-circle",
-            },
-            {
-               type: "checkbox",
-               label: "Checkbox",
-               icon: "fas fa-check-square",
-            },
-            {
-               type: "heading",
-               label: "Heading",
-               icon: "fas fa-heading",
-            },
-            {
-               type: "paragraph",
-               label: "Paragraph",
-               icon: "fas fa-paragraph",
-            },
-            {
-               type: "container",
-               label: "Container",
-               icon: "fas fa-layer-group",
-            },
-         ],
+         fieldTypes,
          formFields: [],
          editingField: null,
          editingFieldRef: null,
